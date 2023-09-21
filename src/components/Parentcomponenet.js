0003b6441d60f75b3d0904ab57a83218950f0989
Mikebrian <mikemunene1173@gmail.com>
2023-09-21 10:37:49 +0300

    code along

---
+++ src/components/Parentcomponenet.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from "./DisplayData";
+
+function ParentComponent() {
+  // ...
+  return (
+    <div>
+      <Form
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+      <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
+  );
+}
+
+export default ParentComponent;
